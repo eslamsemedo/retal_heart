036c9fc46f6bd233fc3ace217e1b37037f51d0ce
eslam2semedo <eslamsemedo@gmail.com>
2025-01-29 19:11:51 +0200

    start

---
+++ retal/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+      <p class="text-blue-600 dark:text-sky-400 text-9xl">The quick brown fox...</p>
+    </>
+  )
+}
+
+export default App
